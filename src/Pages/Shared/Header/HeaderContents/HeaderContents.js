631e69cb65a211d1bfb3951cdb7e6d08cbd1206e
Shamim Reza <wpshamim@gmail.com>
2022-06-09 22:53:44 +0600

    update navbar

--- src/Pages/Shared/Header/HeaderContents/HeaderContents.js
+++ src/Pages/Shared/Header/HeaderContents/HeaderContents.js
@@ -4,7 +4,7 @@ const HeaderContents = () => {
     return (
         <div className="mx-auto text-center text-white leading-8 w-1/2">
             <h5 className="uppercase text-lg tracking-widest">away from monotonous life</h5>
-            <h1 className="text-7xl font-extrabold py-5 ring ring-pink-500 ring-offset-8 my-5">Relax Your Mind</h1>
+            <h1 className="text-7xl font-extrabold py-5">Relax Your Mind</h1>
             <p className="text-lg">If your are looking for blank cassettes on the web, you may be very confused at the difference in price. You may see some for as low as $.17 each.</p>
             <button className="bg-primary hover:bg-primary-500 transition font-semibold px-10 py-3 uppercase text-lg mt-10">Get Started</button>
         </div>
